862bf0e9e282cf151753be696c6ccf80edc393f6
wangyu <451336451@qq.com>
2018-07-31 17:50:36 +0800

    删除多余的

--- src/common/js/fetch.js
+++ src/common/js/fetch.js
@@ -2,18 +2,6 @@ import axios from 'axios'
 
 export default function fetch (config, option) {
   return new Promise((resolve, reject) => {
-    console.log(config)
-    // axios({
-    //   method: 'post',
-    //   url: url,
-    //   params: option
-    // }).then((err, data) => {
-    //   if (!err) {
-    //     resolve(data)
-    //   } else {
-    //     reject(err)
-    //   }
-    // })
     axios(config, (err, data) => {
       if (!err) {
         resolve(data)
