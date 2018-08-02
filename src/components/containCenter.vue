5c2af0054bfdb186e07afbe45e99c7f2b8621064
wxxtql <1072317596@qq.com>
2018-08-02 22:46:29 +0800

    init echarts

--- src/components/containCenter.vue
+++ src/components/containCenter.vue
@@ -1,27 +1,53 @@
 <template>
-  <div class="contain_left">
-    <img src="static/stageData/ct.png">
-  </div>
+  <div class="contain-left" id="map" ref="map"></div>
 </template>
 
 <script>
+require('echarts/lib/chart/map')
+require('echarts/map/js/china')
 export default {
   components: {},
   props: {},
   data () {
+  // 配置数据
     return {
+      mapName: 'china',
+      geoCoordMap: {},
+      name_fontSize: 18,
+      subname_fontSize: 15,
+      name_fontFamily: '等线',
+      nameColor: 'rgb(55, 75, 113)',
+      subname: '数据爬取自千栀网\n，\n上海、浙江无文理科录取人数',
+      name_title: '中国人民大学2017年各省市计划录取人数'
     }
   },
   watch: {},
   computed: {},
   methods: {},
-  created () {},
+  created () {
+    // console.log(this.$echarts)
+    // this.$echarts.sho
+    let myChart = this.$echarts.init(document.getElementById('map'))
+    // 开启加载中
+    myChart.showLoading()
+    // 获取地图数据
+    let mapFeatures = this.$echarts.getMap(this.mapName).geoJson.features
+    myChart.hideLoading()
+    mapFeatures.forEach(v => {
+      // 地区名称
+      var name = v.properties.name
+      // 地区经纬度
+      this.geoCoordMap[name] = v.properties.cp
+    })
+    console.log(this.geoCoordMap)
+  },
   mounted () {}
 }
 </script>
 <style lang='stylus' scoped>
-.contain_left
+.contain-left
   position: relative
+  width 100%
   height: 100%
   >img
     position: absolute
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import getpath from 'common/js/url'
 import router from './router'
 import iView from 'iview'
+import echarts from 'echarts'
 import 'babel-polyfill'
 import 'common/stylus/index.styl'
 import 'iview/dist/styles/iview.css'
@@ -12,7 +13,7 @@ import 'iview/dist/styles/iview.css'
 getpath()
 Vue.use(iView)
 Vue.config.productionTip = false
-
+Vue.prototype.$echarts = echarts
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
